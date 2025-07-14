1d2e4abe112032ff43af236ab20f23940a54b074
mohdashaar <ashaarmomin@gmail.com>
2025-07-14 23:04:38 +0400

    assignments and notes

--- grade.js
+++ grade.js
@@ -1,19 +1,40 @@
-let percrnt = 75.95;
-let grade = "";
+// let percrnt = 75.95;
+// let grade = "";
 
-if (percrnt >= 90){
-    grade = "A"
-}
-else if (percrnt >= 75){
-    grade = "B" 
-}
-else if (percrnt >= 55){
-    grade = "C"
+// if (percrnt >= 90){
+//     grade = "A"
+// }
+// else if (percrnt >= 75){
+//     grade = "B" 
+// }
+// else if (percrnt >= 55){
+//     grade = "C"
     
+// }
+// else if (percrnt >= 40){
+//     grade = "D"
+// } else  {
+//     grade = "E"
+// }
+console.log(`Grade Obtained - ${grade}`);
+let percrnt = 50;
+let grade ="";
+
+if(percrnt >= 90 ){
+    grade ="A"
 }
-else if (percrnt >= 40){
-    grade = "D"
-} else  {
+
+else if (percrnt >= 75){
+    grade = "B"
+}
+
+else if ( percrnt >= 65){
+    grade ="C"
+}
+
+else if (percrnt >= 45){
+    grade = "d"
+} else{
     grade = "E"
 }
-console.log(`Grade Obtained - ${grade}`);
+console.log(`grade obtained - ${grade}`)
